994f6c004c77357e5701a97b5812a5f409127585
Mukul <dhirmukul3@gmail.com>
2020-04-12 08:23:53 +0000

    beryllium: Syberia bringup

--- syberia_beryllium.mk
+++ syberia_beryllium.mk
@@ -6,11 +6,13 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Syberia stuff.
+$(call inherit-product, vendor/syberia/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := syberia_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
